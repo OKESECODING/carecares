61a173f4712eafd516789e17b850b8d9bd65ca24
okese <okesecoding@github>
2022-11-03 20:32:35 +0000

    all complete

--- js/main.js
+++ js/main.js
@@ -1,11 +1,27 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+// /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+// function openNav() {
+//     document.getElementById("mySidebar").style.width = "250px";
+//     document.getElementById("main").style.marginLeft = "250px";
+//   }
   
-  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-  }
+//   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+//   function closeNav() {
+//     document.getElementById("mySidebar").style.width = "0";
+//     document.getElementById("main").style.marginLeft = "0";
+//   }
+
+//   $('.btn-expand-collapse').click(function(e) {
+//     $('.navbar-primary').toggleClass('collapsed');
+// });
+
+// newFunction()
+// function newFunction() {
+//     $('.btn').click(function () {
+//         $('h1').toggleClass('active')
+//     });
+//     ;
+// }
+
+$('.btn').click(function(){
+    $('h1').toggleClass('active');
+});
